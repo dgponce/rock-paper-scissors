af66b69719d5790646ef14f483b757170ceadfe4
Diana Garcia <dg38828@gmail.com>
2022-09-21 13:13:03 -0500

    Add messages to the user

--- app.js
+++ app.js
@@ -52,4 +52,14 @@ startGameBtn.addEventListener('click', () => {
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
   const winner = getWinner(computerChoice, playerChoice);
+  let message = `You picked ${playerChoice}, computer picked ${computerChoice}, therefore you `;
+  if (winner === RESULT_DRAW) {
+    message += 'had a draw.';
+  } else if (winner === RESULT_PLAYER_WINS) {
+    message += 'won.';
+  } else {
+    message += 'lost.';
+  }
+  alert(message);
+  gameIsRunning = false;
 });
